12ba048e10ce71318844d5397324edb3fbe326f9
Olha Kudinova <olgaterehova97@gmail.com>
2023-02-24 08:21:23 +0200

    added const

---
+++ const.js
@@ -0,0 +1,9 @@
+/**
+ * const: Constants are block-scoped such like variables declared using the let keyword.
+ * The value of constant cannot be changed through reasignmrnt and it cannot be redeclared.
+ */ 
+
+ {
+    const a =5;
+    console.log(a); 
+ }
