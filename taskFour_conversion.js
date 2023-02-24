891d54a9f20d27e086828fd85007b5d756a84bcb
Olha Kudinova <olgaterehova97@gmail.com>
2023-02-24 19:35:34 +0200

    updating conversion

--- taskFour_conversion.js
+++ taskFour_conversion.js
@@ -1,29 +1,13 @@
-/**
- * Numeric Conversion: value ('string') is explicitly converted to a number
- */
+/**Numeric Conversion*/
 let str = "567";
 console.log(typeof str); // string
-
 let num = Number(str); // becomes a number 567
 console.log(typeof num);  // number
-
-
-/**
- * String Conversion: value ('boolean') is explicitly converted to a string
- */
+/** String Conversion*/
 let value = true;
 console.log(typeof value); // boolean
-
 value = String(value); // now value is a string "true"
 console.log(typeof value); // string
-
-/**
- * Boolean Conversion: 0, an empty string, null, undefined and NaN 
- * are explicitly converted to fasle. 
- * Other values become true 
- */
-//console.log(Boolean(1) ); // true
-//console.log(Boolean(0) ); // false
-
+/**Boolean Conversion*/
 console.log(Boolean("hello") ); // true
 console.log(Boolean("") ); // false
