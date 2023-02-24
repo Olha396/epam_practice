d43230c2ae742520164e4360e34222db5012a1b1
Olha Kudinova <olgaterehova97@gmail.com>
2023-02-24 08:42:07 +0200

    add dataTypes

---
+++ dataTypes.js
@@ -0,0 +1,34 @@
+/**
+ * Primitives
+ */
+
+let number = 4;
+let string = 'Hello';
+let bollean = true;
+let nullValue = null;
+let sum = Symbol();
+
+/**
+ * String
+ */
+const first = 'Hello';
+const second = 'World';
+const result = first + " " + second;
+console.log(result);
+/**
+ * Number
+ */
+const numberFirst = 1500;
+const numSecond = 0b11;
+
+const x = 2;
+const y = 5;
+const z = x + y;
+console.log(z);
+
+/**
+ * Complex data types, objects and arrays
+ */
+const obj = {
+    name: 'John',
+}
