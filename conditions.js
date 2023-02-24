dd90b39988e99256f41a46a210d61555b4cec0c0
Olha Kudinova <olgaterehova97@gmail.com>
2023-02-24 14:23:07 +0200

    add conditions

---
+++ conditions.js
@@ -0,0 +1,43 @@
+
+
+
+const x = 5;
+
+if (x === 6){
+    console.log('this is true');
+} else{
+    console.log('Else statement');
+}
+
+console.log('I am here');
+
+/**
+18 - 25 ->18%
+25 - 40 ->20%
+41 - 99 ->30%
+*/
+ const age = 20;
+
+ if (age >= 18 && age <= 25){
+    console.log('18%');
+} else if (age >= 26 && age <=40){
+    console.log('20%');
+}else if (age >= 41 && age <=99){
+    console.log('30%');
+}else{
+   
+}
+        
+
+const num = 5;
+
+if (num === 5){
+    console.log('true');
+} else {
+    console.log('false'); 
+}
+
+/**
+ * let's describe it with Ternary operator
+ */
+(num === 5) ? console.log('true') : console.log('false'); 
