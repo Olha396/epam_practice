de7f91044452394e8b4300dce97db981b32329ba
Olha Kudinova <olgaterehova97@gmail.com>
2023-02-24 19:18:25 +0200

    add HT2_1_addition_tovarious_types

---
+++ addition_various_types.js
@@ -0,0 +1,12 @@
+/**
+ * String + Number/
+ */ 
+console.log('foo' + 7) // "foo7"
+/**
+ * String + Boolean
+ */
+console.log('foo' + false) // "foofalse"
+/*
+ *Number + Boolean
+ */
+console.log(1 + true) // 2
