96f3bcf9f6033f84045f4851b49b41e9efbe62bd
Olha Kudinova <olgaterehova97@gmail.com>
2023-02-25 12:51:45 +0200

    add functions_object

---
+++ function_object.js
@@ -0,0 +1,12 @@
+const john = {
+    name: "John",
+    age: 25
+}
+ //console.log(john)
+ //console.log(john.name)
+ //console.log(john["age"])
+ john.language = "JS";
+ delete john.age;
+
+ console.log(john)
+/**Likewise we can remove any property from the object */
