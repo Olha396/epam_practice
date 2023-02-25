b16f7d02437825627bc02f383046bee4f113d94b
Olha Kudinova <olgaterehova97@gmail.com>
2023-02-25 09:30:49 +0200

    add functions_returns

---
+++ functions_return.js
@@ -0,0 +1,14 @@
+/**
+ * Return keyword
+ */
+
+//by defaukt functions return undefined
+
+function sayHey(){
+    console.log('Hey!');
+}
+
+//sayHey();
+
+const result = sayHey();
+console.log('result is:', result);
