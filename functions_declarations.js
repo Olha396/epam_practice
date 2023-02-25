26afe17664baf1026833bca9343b46bdb706d10e
Olha Kudinova <olgaterehova97@gmail.com>
2023-02-25 09:16:07 +0200

    add functions declarations

---
+++ functions_declarations.js
@@ -0,0 +1,15 @@
+/**
+ * Function declarations
+ */
+//keyword 'function'
+//identifier 'sum'
+// parameters 'a' and 'b' (optional)
+// body {}
+// return statements (optional, by default return undefined)
+// HOISTED
+
+function sum (a, b){
+    return a + b;
+}
+const result = sum(5, 10)
+console.log('result is: ', result);
