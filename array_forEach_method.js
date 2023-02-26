72fa7d99b60329481fe87a82ec8c121a0652eb70
Olha Kudinova <olgaterehova97@gmail.com>
2023-02-26 12:06:09 +0200

    added index method

--- array_forEach_method.js
+++ array_forEach_method.js
@@ -1,3 +1,12 @@
-const arrayOfName = ['Alice', 'Jack', 'John', 'Jane', 'Bob'];
+/**Printing names to console */
+//const arrayOfName = ['Alice', 'Jack', 'John', 'Jane', 'Bob'];
 
-arrayOfName.forEach(name => console.log(name));
+//arrayOfName.forEach(name => console.log(name));
+
+/**Index and value */
+    const arrayOfName = ['Alice', 'Jack', 'John', 'Jane', 'Bob'];
+
+    arrayOfName.forEach((name, index) => {
+
+        return console.log(`Element index: ${index} Element value: ${name}`);
+    });
