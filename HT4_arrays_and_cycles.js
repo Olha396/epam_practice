7e96d03a2d5b850efa7a0ade34f7ce084795bfa9
Olha Kudinova <olgaterehova97@gmail.com>
2023-02-26 12:13:27 +0200

    added Task 7

--- HT4_arrays_and_cycles.js
+++ HT4_arrays_and_cycles.js
@@ -3,6 +3,16 @@ const arrayOfMovieName = ['John Wick', 'Oppenheimer', 'Dune', 'Indiana Jones and
 
 arrayOfMovieName.forEach(name => console.log(name));
 
+
+/*Task_7: outputring index of element on the conslole*/
+const arrayOfNumber = [11, 22, 66, 88, 99];
+
+arrayOfNumber.forEach((number, index) => {
+
+    return console.log(`Element index: ${index} Element value: ${number}`);
+});
+
+
 /**Task_8: loop that prints the number "a" less than 10 */
 
 const arr = [1,2,3,4,5,6,7,8,9,10]
