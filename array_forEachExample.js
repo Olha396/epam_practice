3d89724791a2b1fe6287b15327762606dc947810
Olha Kudinova <olgaterehova97@gmail.com>
2023-02-26 10:54:40 +0200

    added example with map

--- array_forEachExample.js
+++ array_forEachExample.js
@@ -1,7 +1,11 @@
 
+    //const arrayOfNumbers = [1, 5, 6, 2, 15];
+    //const arrayOfMultipliedNumbers = [];
+    //arrayOfNumbers.forEach((currentNumber) => arrayOfMultipliedNumbers.push(currentNumber * 2));
+
+    //console.log(arrayOfMultipliedNumbers);
+/**Map: we do not need to make and emplty array */
     const arrayOfNumbers = [1, 5, 6, 2, 15];
-    const arrayOfMultipliedNumbers = [];
-    arrayOfNumbers.forEach((currentNumber) => arrayOfMultipliedNumbers.push(currentNumber * 2));
+    const arrayOfMultipliedNumbers = arrayOfNumbers.map((currentNumber) => currentNumber * 2);
 
     console.log(arrayOfMultipliedNumbers);
-
