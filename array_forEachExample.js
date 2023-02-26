e93f1cfbadc151d196c63da4b348292122bca0f5
Olha Kudinova <olgaterehova97@gmail.com>
2023-02-26 10:40:49 +0200

    add array_forEachExaple

---
+++ array_forEachExample.js
@@ -0,0 +1,7 @@
+
+    const arrayOfNumbers = [1, 5, 6, 2, 15];
+    const arrayOfMultipliedNumbers = [];
+    arrayOfNumbers.forEach((currentNumber) => arrayOfMultipliedNumbers.push(currentNumber * 2));
+
+    console.log(arrayOfMultipliedNumbers);
+
